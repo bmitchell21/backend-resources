4ba9caf16b13e1e3e27265fcabffa296f8ecef43
Brooke <brooked.mitchell@gmail.com>
2020-08-06 04:10:58 -0400

    updated pytest files.

---
+++ lambdafunc/lambda_function.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+
+dynamodb = boto3.client('dynamodb')
+def lambda_handler(event, context):
+    response = dynamodb.update_item(
+        TableName='Res2Counter',
+        Key={
+            'Site': {
+                'N': '0'
+            }
+        },
+        UpdateExpression='SET Visits = Visits + :inc',
+        ExpressionAttributeValues={
+            ':inc': {'N': '1'}
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    res = dynamodb.get_item(
+        TableName='Res2Counter',
+        Key={
+            'Site': {
+                'N': '0'
+            }
+        },
+        ProjectionExpression='Visits',
+    )
+    count = response['Attributes']['Visits']['N']
+    print(res)
+    vCount = int(count)
+    return {
+        'statusCode': 200,
+        'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        },
+        'body': json.dumps(vCount)
+        }
---
+++ tests/unit/test_handler.py
@@ -0,0 +1,13 @@
+import boto3
+import unittest
+import pytest
+import json
+import requests
+
+dynamodb_table_info = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb_table = dynamodb_table_info.Table('Res2Counter')
+
+def test_of_func():
+    response = requests.get("https://4u2lic1984.execute-api.us-east-1.amazonaws.com/Prod/vCount")
+    assert response.status_code == 200
+
