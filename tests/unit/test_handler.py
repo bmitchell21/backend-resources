1fde0336e17339629776c06a172c4659fa69e2f1
Brooke <brooked.mitchell@gmail.com>
2020-08-06 08:54:00 -0400

    updated tests.

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -10,4 +10,5 @@ dynamodb_table = dynamodb_table_info.Table('Res2Counter')
 def test_of_func():
     response = requests.get("https://4u2lic1984.execute-api.us-east-1.amazonaws.com/Prod/vCount")
     assert response.status_code == 200
+    assert response.headers['content-type'] == 'application/json'
 
